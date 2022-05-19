b2f031999415b51848eafa75634d3cb72ffc0afc
leoking2000 <leoking470@gmail.com>
2022-05-19 11:35:10 +0300

    move to vs 2022

--- CG_Project/Game/Game.cpp
+++ CG_Project/Game/Game.cpp
@@ -7,8 +7,17 @@ GL::Engine(1600, 900, "Code along Project", true)
 	//
 	// renderer settings
 	//
+	renderer.light_proj = glm::perspective(glm::radians(90.0f), 1.0f, 0.1f, 1000.0f);
+	renderer.light_view = glm::lookAt(glm::vec3(0.0f, 450.0f, -150.0f), glm::vec3(0.0f, 0.0f, -130.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+	renderer.light_dir = -glm::normalize(glm::vec3(0.0f, 500.0f, -100.0f));
+	renderer.lightSpaceMatrix = renderer.light_proj * renderer.light_view;
+	renderer.shadow_bias = 0.0000005f;
 
-	renderer.clear_color = glm::vec3(0.0f, 0.0f, 0.0f);
+	renderer.fov_angle = glm::radians(45.0f);
+	renderer.min_z = 0.5f;
+	renderer.max_z = 800.0f;
+
+	renderer.clear_color = glm::vec3(0.3f, 0.3f, 0.8f);
 
 	//
 	// Set up game
@@ -22,9 +31,8 @@ void Game::Start()
 	{
 		NewFrame();
 
-		renderer.RenderText(win, 0, 0, "Code along!!!", 40);
-
-
+		renderer.RenderObjects(win, cam.GetCameraView(), objects);
+		renderer.RenderText(win, 0, 0, "Hello World!!!", 40);
 
 		EndFrame();
 	}
